dae8948f2f18dc41258acde8c9a4ac9bcfdf0d38
rudrakshparmar77 <rudrakshparmar77@gmail.com>
2025-07-12 19:40:19 +0000

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBONagyQ3OWJ5QRXaZs4hneJwBrjH4L_q8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "dochelp-e6551.firebaseapp.com",
   projectId: "dochelp-e6551",
   storageBucket: "dochelp-e6551.firebasestorage.app",
